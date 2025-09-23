cf24efc43a949f8faff38b94450ee61b1907fb08
GuHenriquee <gguu.henri@gmail.com>
2025-09-23 16:26:28 -0300

    Operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from objects import UserCreate, UserInDB, Operation, OperationResponse, UserResponse, Login, Token
+from objects import UserCreate, UserInDB, Operation, OperationInDB, UserResponse, Login, Token, OperationResponse
 from operations import Operations
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from database import database
@@ -8,6 +8,9 @@ from contextlib import asynccontextmanager
 from ignore import Gitignore
 from sqlmodel import select
 from login import LoginAndJWT
+from typing import Annotated
+from login import LoginAndJWT
+
 
 
 @asynccontextmanager
@@ -72,23 +75,32 @@ def isLoged(loginCredentiasl: Login, session: database.SessionDep)-> Token:
     return Token(access_token=access_token, token_type="bearer")  #bearer token funciona como dinheiro ou um ingresso de show: qualquer pessoa que o porta (que o possui) tem o direito de usá-lo. O sistema de segurança não precisa de mais nenhuma prova, ele apenas verifica se o token em si é válido.
 
 
-@app.post("/api/operation/", response_model=OperationResponse)
-def operator(operation: Operation):
+@app.post("/api/operation/", status_code=201)
+def operator(operation: Operation, session: database.SessionDep, 
+             current_user: Annotated[UserInDB, Depends(LoginAndJWT.get_current_active_user)])-> OperationResponse:
     
 
-    lastOperation = {
-        "amount":operation.amount,
-        "operationType": operation.operationType,
-        "previousValue": operation.previousValue,
-        "newValue": 0
-    }
-
-    if (lastOperation):
-        if (lastOperation['operationType'] == "Retirar"):
-            lastOperation['newValue'] = Operations.minus( lastOperation['previousValue'], lastOperation['amount'])
+    if (operation):
+        if (operation.operationType == "Retirar"):
+            result = Operations.minus(current_user.patrimony, operation.amount)
         else:
-            lastOperation['newValue'] = Operations.sum( lastOperation['previousValue'], lastOperation['amount'])
+            result = Operations.sum(current_user.patrimony, operation.amount)
+
+    lastOperation = OperationInDB(
+        user_id= current_user.id,
+        amount = operation.amount,
+        operationType = operation.operationType,
+        previousValue = current_user.patrimony,
+        newValue= result
+    )
+
+    current_user.patrimony = lastOperation.newValue
     
+    session.add(lastOperation)
+    session.add(current_user)
+    session.commit()
+    session.refresh(lastOperation)
+    session.refresh(current_user)
 
     return lastOperation
 
